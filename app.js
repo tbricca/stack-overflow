86a6ffe2bd634a8e35693ae10cc80e0b8bd4b43c
tbricca <tbricca@gmail.com>
2019-03-12 20:28:26 -0700

    adding in UI functionality

---
+++ app.js
@@ -0,0 +1,32 @@
+// Question Class: Represents a Question 
+class Question {
+    constructor(text){
+        // take whatever is passed in as parameters and assign to the property of that object
+        this.question = question;
+        this.answer = answer;
+    }
+}
+// UI class: Handle UI Ta sks 
+// such as: display question, add question to list, remove question from list
+    class UI {
+        static displayQuestions() {
+             storedQuestions = [
+                {
+                    question: "What does the constructor function do?",
+                    answer:""
+                },
+                {
+                    question: "How do you define a class in laymans terms?",
+                    answer: "A class defines the shape of a type of object - what methods and properties it has."
+                },
+
+            ]
+        }
+    }
+// Store Class: Handles Storage
+
+// Events: Display Books 
+
+// Events: Add a book
+
+// Remove a book
